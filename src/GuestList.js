995c3144efc2b704821b22a664ad29b63d1a84a3
Steve Mustanski <steve.mustanski@gmail.com>
2018-12-27 14:08:27 -0500

    added PendingGuest component to show ghost tile while guest is being added

--- src/GuestList.js
+++ src/GuestList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Guest from './Guest';
+import PendingGuest from './PendingGuest';
 
 const GuestList = props =>
   <ul>
+    <PendingGuest name={props.pendingGuest} />
     {props.guests
     .filter(guest => !props.isFiltered || guest.isConfirmed)
     .map((guest, index) =>
@@ -27,7 +29,8 @@ GuestList.propTypes = {
   toggleEditingAt: PropTypes.func.isRequired,
   setNameAt: PropTypes.func.isRequired,
   isFiltered: PropTypes.bool.isRequired,
-  removeGuestAt: PropTypes.func.isRequired
+  removeGuestAt: PropTypes.func.isRequired,
+  pendingGuest: PropTypes.string.isRequired
 }
 
 export default GuestList;
