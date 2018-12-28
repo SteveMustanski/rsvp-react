36a046cce180c8004f9ee1d2876f9acd2715e79d
Steve Mustanski <steve.mustanski@gmail.com>
2018-12-28 11:23:56 -0500

    added counter component and updated total invited

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import GuestList from './GuestList';
+import Counter from  './Counter';
 
 class App extends Component {
 
@@ -102,6 +103,7 @@ class App extends Component {
 
 
   render() {
+    const totalInvited = this.getTotalInvited();
     return (
       <div className="App">
         <header>
@@ -126,22 +128,10 @@ class App extends Component {
               /> Hide those who haven't responded
           </label>
           </div>
-          <table className="counter">
-            <tbody>
-              <tr>
-                <td>Attending:</td>
-                <td>2</td>
-              </tr>
-              <tr>
-                <td>Unconfirmed:</td>
-                <td>1</td>
-              </tr>
-              <tr>
-                <td>Total:</td>
-                <td>3</td>
-              </tr>
-            </tbody>
-          </table>
+
+          <Counter 
+          totalInvited = {totalInvited}/>
+
           <GuestList guests={this.state.guests}
             toggleConfirmationAt={this.toggleConfirmationAt}
             toggleEditingAt={this.toggleEditingnAt}
